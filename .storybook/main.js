82cdd2408fcd31fb67f63fab37437d76bc6c5a29
Mark Maksi <artistoflife74@gmail.com>
2022-05-02 04:46:35 +0300

    setting up webpack 5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,5 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
@@ -11,7 +10,10 @@ module.exports = {
     postcss: false,
   },
   framework: "@storybook/react",
+  typescript: {
+    reactDocgen: false,
+  },
   core: {
-    builder: "webpack4",
+    builder: 'webpack5',
   },
 };
